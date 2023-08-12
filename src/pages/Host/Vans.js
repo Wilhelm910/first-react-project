28668ad3bbd31ccd89c118b0ef529cd295b8543a
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2023-08-12 12:39:11 +0200

    host vans details fetch request added

--- src/pages/Host/Vans.js
+++ src/pages/Host/Vans.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Vans() {
+
+    const [vans,setVans] = useState([])
+
+    useEffect(() => {
+        fetch("/api/host/vans")
+            .then(res => res.json())
+            .then(data => console.log(data.vans))
+    }, [])
+
+
     return (
         <h1>Van host page</h1>
     )
